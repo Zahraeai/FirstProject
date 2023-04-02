c37dd90c5451eacd7f7bb0c5b64fd3b64ae9423d
Zahraeai <127307644+Zahraeai@users.noreply.github.com>
2023-04-02 16:56:04 +0430

    Some changes
    Changing settings and Template

--- Config/settings.py
+++ Config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.joinpath('/static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.joinpath('/media')
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+    BASE_DIR/'media',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
